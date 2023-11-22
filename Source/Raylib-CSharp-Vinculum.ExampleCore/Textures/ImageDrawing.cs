46402eb1d26d4ac1c0c40baa5e11ef8a29a26ccc
danilwhale <onefiler1@hotmail.com>
2023-11-22 14:01:15 +0200

    vinculum: update all license headers

--- Source/Raylib-CSharp-Vinculum.ExampleCore/Textures/ImageDrawing.cs
+++ Source/Raylib-CSharp-Vinculum.ExampleCore/Textures/ImageDrawing.cs
@@ -5,7 +5,7 @@
 // This file is licensed to you under the MPL-2.0.
 // See the LICENSE file in the project's root for more info.
 //
-// Raylib-CSharp-Vinculum, bindings for Raylib 4.5.
+// Raylib-CSharp-Vinculum, bindings for Raylib 5.0.
 // Find Raylib-CSharp-Vinculum here: https://github.com/ZeroElectric/Raylib-CSharp-Vinculum
 // Find Raylib here: https://github.com/raysan5/raylib
 //
