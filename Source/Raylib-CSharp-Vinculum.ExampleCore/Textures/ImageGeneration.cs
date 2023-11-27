72f8f18619082d58fbbfaac980df6808028d6e9d
Kenneth M <KennethMizell@outlook.com>
2023-11-27 12:38:14 -0600

    Update ImageGeneration example

--- Source/Raylib-CSharp-Vinculum.ExampleCore/Textures/ImageGeneration.cs
+++ Source/Raylib-CSharp-Vinculum.ExampleCore/Textures/ImageGeneration.cs
@@ -5,7 +5,7 @@
 // This file is licensed to you under the MPL-2.0.
 // See the LICENSE file in the project's root for more info.
 //
-// Raylib-CSharp-Vinculum, bindings for Raylib 4.5.
+// Raylib-CSharp-Vinculum, .Net/C# bindings for raylib 5.0.
 // Find Raylib-CSharp-Vinculum here: https://github.com/ZeroElectric/Raylib-CSharp-Vinculum
 // Find Raylib here: https://github.com/raysan5/raylib
 //
@@ -17,17 +17,19 @@ namespace ZeroElectric.Vinculum.ExampleCore.Textures;
 *
 *   raylib [textures] example - Procedural images generation
 *
-*   This example has been created using raylib 1.8 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*   Example originally created with raylib 1.8, last time updated with raylib 1.8
 *
-*   Copyright (c) 2O17-2021 Wilhem Barbier (@nounoursheureux) and Ramon Santamaria (@raysan5)
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2O17-2023 Wilhem Barbier (@nounoursheureux) and Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
 public unsafe static class ImageGeneration
 {
 
-	const int NUM_TEXTURES = 6;      // Currently we have 7 generation algorithms
+	const int NUM_TEXTURES = 9;      // Currently we have 7 generation algorithms
 
 	public static int main()
 	{
@@ -40,25 +42,36 @@ public unsafe static class ImageGeneration
 		InitWindow(screenWidth, screenHeight, "raylib [textures] example - procedural images generation");
 
 		Image verticalGradient = GenImageGradientLinear(screenWidth, screenHeight, 0, RED, BLUE);
-		Image horizontalGradient = GenImageGradientLinear(screenWidth, screenHeight, 360, RED, BLUE);
+		Image horizontalGradient = GenImageGradientLinear(screenWidth, screenHeight, 90, RED, BLUE);
+		Image diagonalGradient = GenImageGradientLinear(screenWidth, screenHeight, 45, RED, BLUE);
 		Image radialGradient = GenImageGradientRadial(screenWidth, screenHeight, 0.0f, WHITE, BLACK);
+		Image squareGradient = GenImageGradientSquare(screenWidth, screenHeight, 0.0f, WHITE, BLACK);
 		Image checkedImage = GenImageChecked(screenWidth, screenHeight, 32, 32, RED, BLUE);
 		Image whiteNoise = GenImageWhiteNoise(screenWidth, screenHeight, 0.5f);
+		Image perlinNoise = GenImagePerlinNoise(screenWidth, screenHeight, 50, 50, 4.0f);
 		Image cellular = GenImageCellular(screenWidth, screenHeight, 32);
 
 		Texture2D[] textures = new Texture2D[NUM_TEXTURES];
 
 		textures[0] = LoadTextureFromImage(verticalGradient);
 		textures[1] = LoadTextureFromImage(horizontalGradient);
-		textures[2] = LoadTextureFromImage(radialGradient);
-		textures[3] = LoadTextureFromImage(checkedImage);
-		textures[4] = LoadTextureFromImage(whiteNoise);
-		textures[5] = LoadTextureFromImage(cellular);
+		textures[2] = LoadTextureFromImage(diagonalGradient);
+		textures[3] = LoadTextureFromImage(radialGradient);
+		textures[4] = LoadTextureFromImage(squareGradient);
+		textures[5] = LoadTextureFromImage(checkedImage);
+		textures[6] = LoadTextureFromImage(whiteNoise);
+		textures[7] = LoadTextureFromImage(perlinNoise);
+		textures[8] = LoadTextureFromImage(cellular);
 
 		// Unload image data (CPU RAM)
+		UnloadImage(verticalGradient);
+		UnloadImage(horizontalGradient);
+		UnloadImage(diagonalGradient);
 		UnloadImage(radialGradient);
+		UnloadImage(squareGradient);
 		UnloadImage(checkedImage);
 		UnloadImage(whiteNoise);
+		UnloadImage(perlinNoise);
 		UnloadImage(cellular);
 
 		int currentTexture = 0;
@@ -92,12 +105,15 @@ public unsafe static class ImageGeneration
 
 			switch (currentTexture)
 			{
-				case 0: DrawText("VERTICAL GRADIENT", 580, 10, 20, LIGHTGRAY); break;
-				case 1: DrawText("HORIZONTAL GRADIENT", 580, 10, 20, LIGHTGRAY); break;
-				case 2: DrawText("RADIAL GRADIENT", 580, 10, 20, LIGHTGRAY); break;
-				case 3: DrawText("CHECKED", 680, 10, 20, RAYWHITE); break;
-				case 4: DrawText("WHITE NOISE", 640, 10, 20, RED); break;
-				case 5: DrawText("CELLULAR", 670, 10, 20, RAYWHITE); break;
+				case 0: DrawText("VERTICAL GRADIENT", 560, 10, 20, RAYWHITE); break;
+				case 1: DrawText("HORIZONTAL GRADIENT", 540, 10, 20, RAYWHITE); break;
+				case 2: DrawText("DIAGONAL GRADIENT", 540, 10, 20, RAYWHITE); break;
+				case 3: DrawText("RADIAL GRADIENT", 580, 10, 20, LIGHTGRAY); break;
+				case 4: DrawText("SQUARE GRADIENT", 580, 10, 20, LIGHTGRAY); break;
+				case 5: DrawText("CHECKED", 680, 10, 20, RAYWHITE); break;
+				case 6: DrawText("WHITE NOISE", 640, 10, 20, RED); break;
+				case 7: DrawText("PERLIN NOISE", 640, 10, 20, RED); break;
+				case 8: DrawText("CELLULAR", 670, 10, 20, RAYWHITE); break;
 				default: break;
 			}
 
@@ -108,10 +124,13 @@ public unsafe static class ImageGeneration
 		//--------------------------------------------------------------------------------------
 
 		// Unload textures data (GPU VRAM)
-		for (int i = 0; i < NUM_TEXTURES; i++) UnloadTexture(textures[i]);
+		for (int i = 0; i < NUM_TEXTURES; i++)
+		{
+			UnloadTexture(textures[i]);
+		}
 
 		// Close window and OpenGL context
-		CloseWindow();                
+		CloseWindow();
 
 		return 0;
 	}
